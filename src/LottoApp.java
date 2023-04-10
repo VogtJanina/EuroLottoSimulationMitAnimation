b2021d6335064b644d2cddc52fb0bb5d89dff312
Vogt Janina <vogt.janina@gmx.de>
2023-04-10 13:28:27 +0200

    Add MenuBar

--- src/LottoApp.java
+++ src/LottoApp.java
@@ -1,3 +1,4 @@
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
@@ -6,10 +7,16 @@ public class LottoApp {
 	private Display display;
 	private Shell shell;
 	
+	private Menu menuBar; 
+	private MenuItem fileTitle; 
+	private Menu fileMenu; 
+	private MenuItem fileSaveItem; 
+	private MenuItem fileCloseItem; 
+	
 	public LottoApp() {
 		createDisplay();
 		createShell();
-	
+		createMenues();
 		shell.pack(); 
 	}
 	private void createDisplay() {
@@ -20,6 +27,22 @@ public class LottoApp {
 		shell = new Shell(display);
 		GridLayout layout = new GridLayout(1, true); 
 		shell.setLayout(layout);
+	}
+	
+	private void createMenues() {
+		menuBar= new Menu(shell, SWT.BAR); 
+		shell.setMenuBar(menuBar);
+		fileTitle = new MenuItem(menuBar, SWT.CASCADE); 
+		fileTitle.setText("Datei");
+		
+		fileMenu = new Menu(shell, SWT.DROP_DOWN); 
+		fileTitle.setMenu(fileMenu);
+		fileSaveItem = new MenuItem(fileMenu, SWT.PUSH); 
+		//fileSaveItem.setAccelerator(SWT.CTRL + 'S');
+		fileSaveItem.setText("Speichern");
+		fileCloseItem = new MenuItem(fileMenu, SWT.PUSH); 
+		//fileCloseItem.setAccelerator(SWT.CTRL + 'C');
+		fileCloseItem.setText("Beenden");
 	}
 	
 	public void open() {
