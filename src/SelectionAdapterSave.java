41ca531c7b3862e8e6a324393f5d24ececf09a30
Vogt Janina <vogt.janina@gmx.de>
2023-05-01 20:33:12 +0200

    Add win content for save in file

--- src/SelectionAdapterSave.java
+++ src/SelectionAdapterSave.java
@@ -30,11 +30,31 @@ public class SelectionAdapterSave extends SelectionAdapter{
 		FileDialog fileDialog = new FileDialog(parent, SWT.OPEN); 
 		String fileName = fileDialog.open(); 
 		if (fileName != null) {
-			content = msgs.getString("selectedNumbers") +":\n" + msgs.getString("5oo50") + ": " + selected5 + "\n"
-					+ msgs.getString("2oo12") + ": " + selected2 +"\n";
-			content += msgs.getString("drawnNumbers")+ ":\n" + msgs.getString("5oo50") + ": " + numbersDrawing50 + "\n" 
-					+ msgs.getString("2oo12") + ": " + numbersDrawing12;
+			content = msgs.getString("selectedNumbers") +":\n" + msgs.getString("5oo50") + selected5 + "\n"
+					+ msgs.getString("2oo12") + selected2 +"\n";
+			content += msgs.getString("drawnNumbers")+ ":\n" + msgs.getString("5oo50") + numbersDrawing50 + "\n" 
+					+ msgs.getString("2oo12") + numbersDrawing12+"\n";
 			//TODO win/lose
+			int count5oo50 = 0; 
+			int count2oo12 = 0; 
+			for (String s5 : selected5) {
+				for (Integer n5 : numbersDrawing50) {
+					if(n5.toString().equals(s5)) {
+						count5oo50++; 
+					}
+				}
+			}
+			for (String s2 : selected2) {
+				for (Integer n2 : numbersDrawing12) {
+					if(n2.toString().equals(s2)) {
+						count2oo12++; 
+					}
+				}
+			}
+			content += msgs.getString("win") + "\n"
+					+msgs.getString("5oo50") + " " + count5oo50 + "\n"
+					+msgs.getString("2oo12") + " " + count2oo12;
+			
 			FileIO.write(fileName, content);
 		}
 		
